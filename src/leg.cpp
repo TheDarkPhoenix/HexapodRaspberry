5313daa2e3f0bdd25d96b427ea5e6063244fadc6
Maciej Stępień <maciek1284@outlook.com>
2017-11-28 18:31:45 +0100

    usunięte niepotrzebne komentarze

--- src/leg.cpp
+++ src/leg.cpp
@@ -32,14 +32,12 @@ void Leg::calculateJointPoints()
     legJoints.B = Point3f(P11.at<float>(0,0), P11.at<float>(0,1), P11.at<float>(0,2)) + legJoints.A;
 
     Mat P2 = (Mat_<float>(3,1) << lengths.y, 0, 0);
-    //Mat R2 = (Mat_<float>(3,3) << cos(angles.y), -sin(angles.y+angl.z), 0, sin(angles.y+angl.z), cos(angles.y+angl.z), 0, 0, 0, 1);
     Mat R2 = (Mat_<float>(3,3) << cos(angles.y), -sin(angles.y), 0, sin(angles.y), cos(angles.y), 0, 0, 0, 1);
     Mat P22 = R1*(R2*P2);
     P22 = R*P22;
     legJoints.C = Point3f(P22.at<float>(0,0), P22.at<float>(0,1), P22.at<float>(0,2)) + legJoints.B;
 
     Mat P3 = (Mat_<float>(3,1) << lengths.z, 0, 0);
-    //Mat R3 = (Mat_<float>(3,3) << cos(angles.z), -sin(angles.z+angl.z), 0, sin(angles.z+angl.z), cos(angles.z+angl.z), 0, 0, 0, 1);
     Mat R3 = (Mat_<float>(3,3) << cos(angles.z), -sin(angles.z), 0, sin(angles.z), cos(angles.z), 0, 0, 0, 1);
     Mat P33 = R1*(R3*P3);
     P33 = R*P33;
@@ -49,10 +47,6 @@ void Leg::calculateJointPoints()
 int Leg::calculateAngles()
 {
     Point3f newPos = legEnd-legJoints.A; // pozycja poczatku nogi po przekszta³ceniu
-    //newPos.x = abs(newPos.x);
-    //newPos.y = abs(newPos.y);
-    //newPos.z = abs(newPos.z);
-    //cout << newPos << endl;
 
     float lx = lengths.x;
 
@@ -75,18 +69,13 @@ int Leg::calculateAngles()
     relAngles.y = angles.y;
     relAngles.z = b;
 
-    //if(angles.x < -1.5 || angles.x > 1.5 || angles.y < -1.5 || angles.y > 1.5 || angles.z < -1.5 || angles.z > 1.5 || angles.x != angles.x || angles.y != angles.y || angles.z!=angles.z)//nan detect
     if(angles.x != angles.x || angles.y != angles.y || angles.z!=angles.z)//nan detect
     {
         calculateJointPoints();
         return -1;
-        /*angles = initAngles;
-        calculateJointPoints(angl);
-        legEnd = legJoints.D;*/
     }
 
     calculateJointPoints();
-    //cout << angles << endl;
     calculateServoSignals();
     return 1;
 }
@@ -98,8 +87,7 @@ void Leg::calculateServoSignals()
     sygnalA = relAngles.x*wspolczynnik + signals.x;
     sygnalB = -relAngles.y*wspolczynnik + signals.y;
     sygnalC = (CV_PI/2 -relAngles.z)*wspolczynnik + signals.z;
-    //cout << sygnalC << endl;
-    //cout << -(CV_PI/2 -angles.z)*wspolczynnik  << endl;
+
     if(device)
     {
         device->setTarget(servos.x, sygnalA);
